7b32c6573cd094ceec9bb946aebc1c9db16b4a81
asimfahim <43362909+asimfahim@users.noreply.github.com>
2018-09-30 19:22:14 +0600

    Add files via upload
    Form Application

---
+++ FORM/form/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+    <head>
+        <title>
+            Login Form
+        </title>
+    </head>
+    <body>
+        <form name = "index" method = "get" action = "login-handler.php">
+            <center>
+            User Name<br>
+            <input type="text" name="username"/>
+            <br>
+            Password<br>
+            <input type = "text" name = "password"/>
+            <br><br>
+            <input type = "reset" name = "reset" value = "reset"/>
+			<br>
+			<input type = "submit" name = "submit" value = "submit"/> 
+            <br>
+            <br>
+            <a href="https://www.facebook.com">
+                <img src = "fb.jpg" alt = "facebook" style="width:200px;height:42px;border:0">
+            </a>
+            <a href="https://www.gmail.com">
+                <img src = "gmail.jpg" alt = "facebook" style="width:200px;height:42px;border:0">
+            </a>
+            <br><br>
+            
+              
+                  
+            </center>
+        </form>
+    </body>
+</html>
---
+++ FORM/form/login-handler.php
@@ -0,0 +1,7 @@
+<?php
+
+$un = $_GET['username'];
+$pw = $_GET['password'];
+echo "user name: $un  and password: $pw";
+
+?>
